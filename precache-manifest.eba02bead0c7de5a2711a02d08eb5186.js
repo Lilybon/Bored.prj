09b7543cc903fd93d81dc633c14e5c56aa763bc7
Lilybon <zebra10029@gmail.com>
2019-07-22 13:04:46 +0800

    Deploy

---
+++ precache-manifest.eba02bead0c7de5a2711a02d08eb5186.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "363db54bcc593038e218",
+    "url": "/Bored.prj/css/chunk-a81c48e0.4ecddd9d.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Bored.prj/robots.txt"
+  },
+  {
+    "revision": "363db54bcc593038e218",
+    "url": "/Bored.prj/js/chunk-a81c48e0.b00e20a1.js"
+  },
+  {
+    "revision": "06ff42f79145c8d70756",
+    "url": "/Bored.prj/css/chunk-088a12ac.927240d6.css"
+  },
+  {
+    "revision": "1d864f1712bb6a4ed9d7",
+    "url": "/Bored.prj/css/chunk-31fa9c29.706a84e9.css"
+  },
+  {
+    "revision": "1d864f1712bb6a4ed9d7",
+    "url": "/Bored.prj/js/chunk-31fa9c29.1614ec90.js"
+  },
+  {
+    "revision": "4b4bb6139459719752ca",
+    "url": "/Bored.prj/css/chunk-32edecc8.dad6d300.css"
+  },
+  {
+    "revision": "4b4bb6139459719752ca",
+    "url": "/Bored.prj/js/chunk-32edecc8.89ca7f43.js"
+  },
+  {
+    "revision": "77d02a5e4369d0c30a8e",
+    "url": "/Bored.prj/js/app.e36eb3be.js"
+  },
+  {
+    "revision": "06ff42f79145c8d70756",
+    "url": "/Bored.prj/js/chunk-088a12ac.412b67fa.js"
+  },
+  {
+    "revision": "5d97919f56f9292bc20b",
+    "url": "/Bored.prj/css/chunk-f34bda26.3abd6125.css"
+  },
+  {
+    "revision": "5d97919f56f9292bc20b",
+    "url": "/Bored.prj/js/chunk-f34bda26.ad659d90.js"
+  },
+  {
+    "revision": "b7e3bbbb50411c753c8c",
+    "url": "/Bored.prj/css/chunk-vendors.724e6a1c.css"
+  },
+  {
+    "revision": "b7e3bbbb50411c753c8c",
+    "url": "/Bored.prj/js/chunk-vendors.84753006.js"
+  },
+  {
+    "revision": "647865d266756f22a14193790300fbf5",
+    "url": "/Bored.prj/index.html"
+  },
+  {
+    "revision": "77d02a5e4369d0c30a8e",
+    "url": "/Bored.prj/css/app.113a6629.css"
+  }
+];
